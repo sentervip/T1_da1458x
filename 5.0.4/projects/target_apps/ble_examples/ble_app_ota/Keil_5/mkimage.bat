d23dacd6fc80185f6c3c7dd913fc874fb727e0a7
aizj <aizj@marvoto.com>
2020-03-18 17:46:00 +0800

    remove all

--- 5.0.4/projects/target_apps/ble_examples/ble_app_ota/Keil_5/mkimage.bat
+++
@@ -1,3 +0,0 @@
-C:\Keil_v5\ARM\ARMCC\BIN\hex2bin.exe .\out_583\ble_app_ota_583.hex 
-C:\Keil_v5\ARM\ARMCC\BIN\mkimage.exe single .\out_583\ble_app_ota_583.bin ble_580_sw_version.h ble_app_ota.img
-C:\Keil_v5\ARM\ARMCC\BIN\mkimage.exe multi spi secondary_bootloader.bin ble_app_ota.img 0x8000 ble_app_ota.img 0x13000 0x1f000 ble_app_ota_all.bin
